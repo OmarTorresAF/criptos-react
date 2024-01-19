f5887ce187430a8bf350e69b0a4c420c1d8cf9ef
TDevtechs <torresdevtech@gmail.com>
2024-01-19 11:54:38 -0600

    Style component added in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
 
+const Heading = styled.h1 `
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+`
 
 
 function App() {
 
   return (
-   <h2>desde app</h2>
+   <Heading>Desde app</Heading>
       
   )
   }
